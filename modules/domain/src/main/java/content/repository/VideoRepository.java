85173ca7c609330c00b679e0fcdd5a2a02631a17
JalHaeHyo <sangok237@gmail.com>
2026-02-16 08:41:47 +0900

    feat: 재생 정보 조회 api 구현

--- modules/domain/src/main/java/content/repository/VideoRepository.java
+++ modules/domain/src/main/java/content/repository/VideoRepository.java
@@ -2,7 +2,9 @@ package content.repository;
 
 import content.entity.Video;
 import content.entity.WatchHistory;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VideoRepository extends JpaRepository<Video, Long> {
+  Optional<Video> findByContentIdAndEpisodeNo(Long contentId, Integer episodeNo);
 }
