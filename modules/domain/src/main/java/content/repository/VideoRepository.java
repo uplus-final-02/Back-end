50997ab8ee4b2d780df39b2726455d22bbcfe497
ChoiBoKeun1 <cbk121779@gmail.com>
2026-02-15 21:03:54 +0900

    Revert "feat: 컨텐츠 관련 dto, service, controller 생성"
    This reverts commit 353948244c2a4a2a97c8772179be1e142dd35ddb.

--- modules/domain/src/main/java/content/repository/VideoRepository.java
+++ modules/domain/src/main/java/content/repository/VideoRepository.java
@@ -1,12 +1,8 @@
 package content.repository;
 
-import aj.org.objectweb.asm.commons.Remapper;
 import content.entity.Video;
 import content.entity.WatchHistory;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VideoRepository extends JpaRepository<Video, Long> {
-
-  Optional<Video> findByContentIdAndEpisodeNo(Long contentId, Integer episodeNo);
 }
