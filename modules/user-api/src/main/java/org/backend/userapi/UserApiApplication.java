0e27537444abca81060de7311af0221b6f94c7ef
ChoiBoKeun1 <cbk121779@gmail.com>
2026-02-15 23:03:50 +0900

    fix: user-api 에 content 패키지 entity, repository 스캔 경로 추가

--- modules/user-api/src/main/java/org/backend/userapi/UserApiApplication.java
+++ modules/user-api/src/main/java/org/backend/userapi/UserApiApplication.java
@@ -2,10 +2,22 @@ package org.backend.userapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "org.backend")
+@EnableJpaRepositories(basePackages = {
+    "org.backend",
+    "content.repository"
+})
+@EntityScan(basePackages = {
+    "org.backend",
+    "content.entity"
+})
 public class UserApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApiApplication.class, args);
     }
 }
+
+
