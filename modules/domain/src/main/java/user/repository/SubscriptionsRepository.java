8628434fc13c77418e3b385e7e65eae92a937d68
eunii2 <chb7562@gmail.com>
2026-02-26 16:13:47 +0900

    feat(admin): 사용자 상세 조회 API 구현 (구독/결제 포함)

--- modules/domain/src/main/java/user/repository/SubscriptionsRepository.java
+++ modules/domain/src/main/java/user/repository/SubscriptionsRepository.java
@@ -2,12 +2,9 @@ package user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import user.entity.Subscriptions;
-import common.enums.UserStatus;
 
 import java.util.Optional;
 
 public interface SubscriptionsRepository extends JpaRepository<Subscriptions, Long> {
-  boolean existsByUserIdAndStatus(Long userId, UserStatus status);
-
-  Optional<Subscriptions> findByUserId(Long userId);
+  Optional<Subscriptions> findByUser_Id(Long userId);
 }
