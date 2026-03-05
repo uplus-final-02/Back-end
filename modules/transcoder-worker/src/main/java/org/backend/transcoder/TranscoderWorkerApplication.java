02842f3a9f1786edbda7ce7a1d4121c11e928268
eunii2 <chb7562@gmail.com>
2026-03-05 10:26:01 +0900

    fix(transcoder): 컴포넌트 스캔 범위 설정

--- modules/transcoder-worker/src/main/java/org/backend/transcoder/TranscoderWorkerApplication.java
+++ modules/transcoder-worker/src/main/java/org/backend/transcoder/TranscoderWorkerApplication.java
@@ -2,8 +2,22 @@ package org.backend.transcoder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@EnableJpaAuditing
+@SpringBootApplication(scanBasePackages = {
+        "org.backend.transcoder",
+        "core",
+        "common", "content"
+})
+@EntityScan(basePackages = {
+        "content.entity"
+})
+@EnableJpaRepositories(basePackages = {
+        "content.repository"
+})
 public class TranscoderWorkerApplication {
     public static void main(String[] args) {
         SpringApplication.run(TranscoderWorkerApplication.class, args);
