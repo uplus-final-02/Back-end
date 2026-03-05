5634961810e859b0cc7eb1c1191bb7082f0911b2
eunii2 <chb7562@gmail.com>
2026-03-05 17:24:18 +0900

    fix(admin&transcoder): JacksonConfig 추가

---
+++ modules/admin-api/src/main/java/org/backend/admin/config/JacksonConfig.java
@@ -0,0 +1,17 @@
+package org.backend.admin.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper om = new ObjectMapper();
+        om.registerModule(new JavaTimeModule());
+        return om;
+    }
+}
