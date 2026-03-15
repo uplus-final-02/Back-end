e36ddd5d357d0f4efb7a1695fa930550efca9dd2
eunii2 <chb7562@gmail.com>
2026-03-15 21:09:59 +0900

    fix(user): 사용자 업로드 api url 수정

--- modules/user-api/src/main/java/org/backend/userapi/upload/controller/UserUploadController.java
+++ modules/user-api/src/main/java/org/backend/userapi/upload/controller/UserUploadController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/user/uploads")
+@RequestMapping("api/user/uploads")
 public class UserUploadController {
 
     private final UserUploadDraftService draftService;
